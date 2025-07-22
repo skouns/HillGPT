b8e8c3c5584dd1f462001c18d9781cf15edc7e47
Sinclair Kouns <sinclairkouns@Sinclairs-MacBook-Pro.local>
2025-07-22 00:24:06 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🚀 StaffGPT</h1>
+      <p>Vercel deploy successful. UI coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
